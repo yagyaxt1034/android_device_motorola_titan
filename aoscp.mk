eba15867f61d2ab4336544b292c8a808998fd5db
RenanQueiroz <queirozrrq@gmail.com>
2017-09-03 02:23:40 -0400

    AOSCP Bringup

--- aoscp.mk
+++ aoscp.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/titan/full_titan.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common CypherOS stuff.
+$(call inherit-product, vendor/aoscp/configs/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := titan
-PRODUCT_NAME := lineage_titan
+PRODUCT_NAME := aoscp_titan
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
