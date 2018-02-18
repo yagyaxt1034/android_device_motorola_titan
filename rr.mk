f39d4222a2b81c11b10cb237d947af21da82e638
RenanQueiroz <queirozrrq@gmail.com>
2018-02-17 21:40:36 -0500

    Specify bootanimation resolution

--- rr.mk
+++ rr.mk
@@ -3,6 +3,8 @@ $(call inherit-product, device/motorola/titan/full_titan.mk)
 # Inherit some common RR stuff.
 $(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
+TARGET_BOOT_ANIMATION_RES := 720
+
 PRODUCT_RELEASE_NAME := titan
 PRODUCT_NAME := rr_titan
 
